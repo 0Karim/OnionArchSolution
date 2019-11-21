6cec4b5e520d00c7b7a5fddac9947d31ece47833
kareem.alaa <karimalaa298@gmail.com>
2019-11-21 14:20:03 +0200

    Add Partial View for ModalHeader , ModalFooter

---
+++ OA.UIMVC/Models/BootstrapModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OA.UIMVC.Models
+{
+    public class BootstrapModel
+    {
+        public string ID { get; set; }
+
+        public string AreaLabeledId { get; set; }
+        
+        public string Size { set; get; }
+    }
+}
---
+++ OA.UIMVC/Models/ModalFooter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OA.UIMVC.Models
+{
+    public class ModalFooter
+    {
+        public string ButtonName { get; set; }
+    }
+}
